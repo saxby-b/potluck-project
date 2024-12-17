babc7329a99e420e40ef3e58c22a6403ecc76944
C. Saxby Bain <csaxbybain@yahoo.com>
2024-12-17 14:00:42 -0500

    Added capability to assign food

--- js/script.js
+++ js/script.js
@@ -11,6 +11,10 @@ const guestCount = document.querySelector(".attendance");
 // alert when guest list is full (not yet visible)
 const guestFull = document.querySelector(".alert");
 
+const assignButton = document.querySelector(".assign");
+
+const assignedItems = document.querySelector(".assigned-items");
+
 addGuestButton.addEventListener("click", function () {
     const guest = guestInput.value;
     //console.log(guest);
@@ -38,7 +42,7 @@ const addToList = function (guest) {
 const updateGuestCount = function () {
     let guests = document.querySelectorAll(".guest-list li");
     guestCount.innerText = guests.length;
-    
+
     if (guests.length === 8) {
         addGuestButton.classList.add("hide");
         guestInput.classList.add("hide");
@@ -46,3 +50,23 @@ const updateGuestCount = function () {
         guestFull.classList.remove("hide");
     }
 }
+
+const assignItems = function () {
+    const potluckItems = ["ham and cheese sliders", "cookies","caprese skewers", "Neiman Marcus bars", 
+        "corn dip", "key lime icebox cake", "pico de gallo", "cheeseburger sliders", "cucumber salad", "chicken sliders",
+         "lemon crumb bars", "stromboli"];
+    
+    const allGuests = document.querySelectorAll(".guest-list li");
+    for (let guest of allGuests) {
+        let randomPotluckIndex = Math.floor(Math.random() * potluckItems.length);
+        let randomPotluckItem = potluckItems[randomPotluckIndex];
+
+        let listItem = document.createElement("li");
+        listItem.innerText = `${guest.innerText} is bringing ${randomPotluckItem}. `;
+        assignedItems.append(listItem);
+    }
+}
+
+assignButton.addEventListener("click", function() {
+    assignItems();
+})
